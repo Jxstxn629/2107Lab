fb332199b884f2ad4856546544706d78ca830055
Jxstxn <smellypudding@gmail.com>
2020-01-22 19:29:31 +0800

    Lab2 IP

---
+++ Lab2/RMIServerApp/src/rmiserverapp/StudentServant.java
@@ -0,0 +1,18 @@
+package rmiserverapp;
+import interfaces.SITStudent;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class StudentServant extends UnicastRemoteObject  implements SITStudent {
+	public StudentServant() throws RemoteException {
+		super();     
+		}     
+	@Override 
+	public String greet(String name) throws RemoteException {
+		return "Welcome " + name + " to RMI tutorial!";
+		}     
+	@Override 
+	public int add(int x, int y) throws RemoteException {
+		return x + y;     
+		} 
+	}
